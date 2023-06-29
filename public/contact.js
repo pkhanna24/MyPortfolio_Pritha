f013c2844c528dbe0dcbe479a8bef8962d5e7d0c
pkhanna24 <102728325+pkhanna24@users.noreply.github.com>
2023-06-29 15:07:24 +0530

    Add files via upload

---
+++ public/contact.js
@@ -0,0 +1,105 @@
+import React, { useRef } from "react";
+import emailjs from "emailjs-com";
+import styled from "styled-components";
+//service_09i5z6s service_id
+// 6UpV1XvnDfOslHGpk user_id
+// template_arzlxnz template_id
+
+const useContactForm = () => {
+  const formRef = React.useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_09i5z6s",
+        "template_arzlxnz",
+        formRef.current.form,
+        "6UpV1XvnDfOslHGpk"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          console.log("message sent");
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  return { formRef, sendEmail };
+};
+
+const Contact = () => {
+  const { formRef, sendEmail } = useContactForm();
+
+  return (
+    <StyledContactForm>
+      <form ref={formRef} onSubmit={sendEmail}>
+        <label>Name</label>
+        <input type="text" name="user_name" />
+        <label>Email</label>
+        <input type="email" name="user_email" />
+        <label>Message</label>
+        <textarea name="message" />
+        <input type="submit" value="Send" />
+      </form>
+    </StyledContactForm>
+  );
+};
+export default Contact;
+
+const StyledContactForm = styled.div`
+  width: 400px;
+
+  form {
+    display: flex;
+    align-items: flex-start;
+    flex-direction: column;
+    width: 100%;
+    font-size: 16px;
+
+    input {
+      width: 100%;
+      height: 35px;
+      padding: 7px;
+      outline: none;
+      border-radius: 5px;
+      border: 1px solid rgb(220, 220, 220);
+
+      &:focus {
+        border: 2px solid rgba(0, 206, 158, 1);
+      }
+    }
+
+    textarea {
+      max-width: 100%;
+      min-width: 100%;
+      width: 100%;
+      max-height: 100px;
+      min-height: 100px;
+      padding: 7px;
+      outline: none;
+      border-radius: 5px;
+      border: 1px solid rgb(220, 220, 220);
+
+      &:focus {
+        border: 2px solid rgba(0, 206, 158, 1);
+      }
+    }
+
+    label {
+      margin-top: 1rem;
+    }
+
+    input[type="submit"] {
+      margin-top: 2rem;
+      cursor: pointer;
+      background: rgb(249, 105, 14);
+      color: white;
+      border: none;
+    }
+  }
+};
